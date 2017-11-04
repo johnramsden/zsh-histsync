31a4b1e19ee4bff5b87a748e3cf88957a5c07c7d
John Ramsden <johnramsden@users.noreply.github.com>
2017-11-04 16:23:20 -0700

    Add proper usage and README

--- README.md
+++ README.md
@@ -6,9 +6,83 @@ Synchronizes history between multiple machines using a git repository.
 
 ## Setup
 
+Create a git repository, or clone one in the location your history will be kept.
 
-Create a git repository, or clone one in the location your history will be kept. The default location is ```${ZSH_CUSTOM}/zsh-history/```. This expands to ```${HOME}/.oh-my-zsh/custom/``` by default, but can be changed in the ```zshrc```.
+The default location is ```${ZSH_CUSTOM}/zsh-history/```. This will depend on where your ```${ZSH_CUSTOM}``` directory is, it is located at ```${HOME}/.oh-my-zsh/custom/``` by default, but can be changed in the ```zshrc```.
 
-To override the default location, add ```export ZSH_HISTSYNC_REPO=<desired location>``` in your ```zshrc``` configuration file.
+```
+cd ${ZSH_CUSTOM}
+git init zsh-history
+```
+
+Add a remote location where history will be kept, and copy in your history you want to start with. Alternate Lee create a blank file to start with empty history
+
+```
+git remote add origin git@github.com:johnramsden/zsh-history.git
+cp ${HOME}/.zsh_history zsh_history
+git add zsh_history
+```
+
+With the history set up, clone the plugin in your oh-my-zsh plugin directory.
+
+```
+cd ${ZSH_CUSTOM}/plugins
+git clone git@github.com:johnramsden/zsh-history.git
+```
+
+Now add the plugin to your zshrc active plugins, it should be added as 'histsync'.
+
+```
+plugins=(git archlinux ... histsync)
+```
+
+## Options
+
+To override the default options, export the associated variable in your ```zshrc``` configuration file.
+
+*   ```ZSH_HISTSYNC_FILE_NAME=zsh_history```           - Default settings, overrride in zshrc
+*   ```ZSH_HISTSYNC_REPO=${ZSH_CUSTOM}/zsh-history```  - Set zsh histsync repo location
+*   ```GIT_COMMIT_MSG="History update from $(hostname) - $(date)"```  - Set commit message
 
 ## Usage
+
+The ```histsync``` command is used to manage the history synchronization.
+
+The following commands exist:
+
+*   ```histsync```
+*   ```histsync clone```
+*   ```histsync commit <repository>```
+*   ```histsync help```
+*   ```histsync pull```
+*   ```histsync push```
+
+### Main Command
+
+Command    | Description
+-----------|---
+histsync   | Merge local history with remote, and push resulting history
+
+### Subcommands
+
+Subcommands can be used to do individual steps on the repository.
+
+Format: ```histsync [subcommand]```
+
+Command                 | Description
+------------------------|---
+```clone <repository>```      | Clone a repository to ZSH_HISTSYNC_REPO
+```commit```                  | Commit the current history file
+```help```                    | Print usage information
+```pull```                    | Pull from remote and merge current history
+```push```                    | Push committed history to remote
+
+### Variables
+
+To override the default options, export the associated variable in your zshrc
+
+Variable                 | Description
+------------------------|---
+```ZSH_HISTSYNC_FILE_NAME```  | Name to save history as in repository
+```ZSH_HISTSYNC_REPO```       | zsh histsync repo location
+```GIT_COMMIT_MSG```          | Commit message
--- histsync.plugin.zsh
+++ histsync.plugin.zsh
@@ -1,71 +1,94 @@
 ###
 # John Ramsden
 # johnramsden @ github
-# Version 0.1.0
+# Version 0.1.1
 ###
 
 autoload -U colors
 colors
 
-# Default settings, overrride in zshrc
-ZSH_HISTSYNC_FILE_NAME=zsh_history
+## Settings ##
+# The following variables can be exported in your zshrc to override the defaults:
 
-# Set zsh histsync location to XDG config directory if set
-ZSH_HISTSYNC_REPO=${ZSH_CUSTOM}/zsh-history
+ZSH_HISTSYNC_FILE_NAME=zsh_history           # Default settings, overrride in zshrc
+ZSH_HISTSYNC_REPO=${ZSH_CUSTOM}/zsh-history  # Set zsh histsync repo location
+GIT_COMMIT_MSG="History update from $(hostname) - $(date)"  # Set commit message
 
-ZSH_HISTSYNC_FILE=${ZSH_HISTSYNC_REPO}/${ZSH_HISTSYNC_FILE_NAME}
+## Local variables and functions
 
-GIT_COMMIT_MSG="History update from $(hostname) - $(date)"
+local ZSH_HISTSYNC_FILE=${ZSH_HISTSYNC_REPO}/${ZSH_HISTSYNC_FILE_NAME}
+
+function histsync_help_output() {
+  printf "%-25s | %-50s\n" "  ${1}" "${2}"
+}
 
 function histsync_usage() {
-    echo "${bold_color}${fg[red]}Usage:${reset_color}" 1>&2;
+  echo "histsync usage"
+  echo
+  echo "-------------------------------------------------------------"
+  echo "  histsync [subcommand]"
+  echo
+  echo "-------------------------------------------------------------"
+  echo "Main command:"
+  histsync_help_output "histsync" "Merge local history with remote, and push resulting history"
+  echo
+  echo "-------------------------------------------------------------"
+  echo "Sub commands:"
+  histsync_help_output "clone <repository>" "Clone a repository to ZSH_HISTSYNC_REPO"
+  histsync_help_output "commit" "Commit the current history file"
+  histsync_help_output "help"   "Print usage information"
+  histsync_help_output "pull"   "Pull from remote and merge current history"
+  histsync_help_output "push"   "Push committed history to remote"
 
-    echo "histsync"
-    echo "histsync clone"
-    echo "histsync commit <repository>"
-    echo "histsync help"
-    echo "histsync pull"
-    echo "histsync push"
+  echo
+  echo "-------------------------------------------------------------"
+  echo "To override the default options, export the associated variable in your zshrc"
+  echo
+  histsync_help_output "ZSH_HISTSYNC_FILE_NAME" "Name to save history as in repository"
+  histsync_help_output 'ZSH_HISTSYNC_REPO'      "zsh histsync repo location"
+  histsync_help_output 'GIT_COMMIT_MSG'         "Commit message"
 
-    return 0;
+  echo
+
+  return 0;
 }
 
 # Pull current master and merge with zsh_history
 function histsync_pull() {
-    local DIR=${CWD}
+  local DIR=${CWD}
 
-    # Pull
-    cd ${ZSH_HISTSYNC_REPO} && git pull
-    if [[ ${?} != 0 ]]; then
-        echo "${bold_color}${fg[red]}Failed to pull from git repo...${reset_color}";
-        cd ${DIR}
-        return 1
-    fi
-
-    # Merge
-    cat ${HISTFILE} ${ZSH_HISTSYNC_FILE_NAME} | awk '/:[0-9]/ { if(s) { print s } s=$0 } !/:[0-9]/ { s=s""$0 } END { print s }' | sort -n > "${ZSH_HISTSYNC_FILE}.merged"
-    local MERGE_SUCCESS=${?}
+  # Pull
+  cd ${ZSH_HISTSYNC_REPO} && git pull
+  if [[ ${?} != 0 ]]; then
+    echo "${bold_color}${fg[red]}Failed to pull from git repo...${reset_color}";
     cd ${DIR}
+    return 1
+  fi
 
-    if [[ ${MERGE_SUCCESS} != 0 ]]; then
-        echo "${bold_color}${fg[red]}Failed to merge histories...${reset_color}";
-        return 1
-    fi
+  # Merge
+  cat ${HISTFILE} ${ZSH_HISTSYNC_FILE_NAME} | awk '/:[0-9]/ { if(s) { print s } s=$0 } !/:[0-9]/ { s=s""$0 } END { print s }' | sort -u > "${ZSH_HISTSYNC_FILE}.merged"
+  local MERGE_SUCCESS=${?}
+  cd ${DIR}
 
-    # Backup
-    echo "Backing up old history and applying new merged history"
-    cp --backup=simple "${ZSH_HISTSYNC_FILE}.merged" "${HISTFILE}"
-    rm "${ZSH_HISTSYNC_FILE}.merged"
+  if [[ ${MERGE_SUCCESS} != 0 ]]; then
+    echo "${bold_color}${fg[red]}Failed to merge histories...${reset_color}";
+    return 1
+  fi
 
-    local BACKUP_SUCCESS=${?}
-    cd ${DIR}
+  # Backup
+  echo "Backing up old history and applying new merged history"
+  cp --backup=simple "${ZSH_HISTSYNC_FILE}.merged" "${HISTFILE}"
+  rm "${ZSH_HISTSYNC_FILE}.merged"
 
-    if [[ ${BACKUP_SUCCESS} != 0 ]]; then
-        echo "${bold_color}${fg[red]}Failed to backup and apply history...${reset_color}";
-        return 1
-    fi
+  local BACKUP_SUCCESS=${?}
+  cd ${DIR}
 
-    return 0
+  if [[ ${BACKUP_SUCCESS} != 0 ]]; then
+    echo "${bold_color}${fg[red]}Failed to backup and apply history...${reset_color}";
+    return 1
+  fi
+
+  return 0
 }
 
 function histsync_commit() {
@@ -82,12 +105,12 @@ function histsync_commit() {
   cd ${DIR}
 
   if [[ ${COMMIT_SUCCESS} != 0 ]]; then
-      echo "${bold_color}${fg[red]}Failed to commit to git repo...${reset_color}";
-      return 1
+    echo "${bold_color}${fg[red]}Failed to commit to git repo...${reset_color}";
+    return 1
   elif [[ ${BACKUP_SUCCESS} != 0 ]]; then
-      echo "${bold_color}${fg[red]}Failed backup and commit history...${reset_color}";
-      cd ${DIR}
-      return 1
+    echo "${bold_color}${fg[red]}Failed backup and commit history...${reset_color}";
+    cd ${DIR}
+    return 1
   fi
 
   return 0
@@ -105,8 +128,8 @@ function histsync_push() {
   cd ${DIR}
 
   if [[ ${PUSH_SUCCESS} != 0 ]]; then
-      echo "${bold_color}${fg[red]}Failed to push to git repo...${reset_color}";
-      return 1
+    echo "${bold_color}${fg[red]}Failed to push to git repo...${reset_color}";
+    return 1
   fi
   return 0
 }
@@ -122,8 +145,8 @@ function histsync_clone() {
   git clone "${1}" "${ZSH_HISTSYNC_REPO}"
 
   if [[ ${?} != 0 ]]; then
-      echo "${bold_color}${fg[red]}Failed to clone git repo...${reset_color}";
-      return 1
+    echo "${bold_color}${fg[red]}Failed to clone git repo...${reset_color}";
+    return 1
   fi
   return 0
 }
@@ -131,20 +154,20 @@ function histsync_clone() {
 function histsync_sync() {
   histsync_pull
   if [[ ${?} != 0 ]]; then
-      echo "${bold_color}${fg[red]}Failed to pull from git repo...${reset_color}";
-      return 1
+    echo "${bold_color}${fg[red]}Failed to pull from git repo...${reset_color}";
+    return 1
   fi
 
   histsync_commit
   if [[ ${?} != 0 ]]; then
-      echo "${bold_color}${fg[red]}Failed commit to git repo...${reset_color}";
-      return 1
+    echo "${bold_color}${fg[red]}Failed commit to git repo...${reset_color}";
+    return 1
   fi
 
   histsync_push
   if [[ ${?} != 0 ]]; then
-      echo "${bold_color}${fg[red]}Failed to push to git repo...${reset_color}";
-      return 1
+    echo "${bold_color}${fg[red]}Failed to push to git repo...${reset_color}";
+    return 1
   fi
 
   return 0
@@ -154,23 +177,23 @@ function histsync() {
   command="${1:-sync}"
 
   case ${command} in
-              clone )
-              histsync_clone "${2}"
-              ;;
-              commit )
-              histsync_commit
-              ;;
-              pull )
-              histsync_pull
-              ;;
-              push )
-              histsync_push
-              ;;
-              sync )
-              histsync_sync
-              ;;
-              * )
-              histsync_usage
-              ;;
-          esac
+    clone )
+      histsync_clone "${2}"
+      ;;
+    commit )
+      histsync_commit
+      ;;
+    pull )
+      histsync_pull
+      ;;
+    push )
+      histsync_push
+      ;;
+    sync )
+      histsync_sync
+      ;;
+    * )
+      histsync_usage
+      ;;
+  esac
 }
